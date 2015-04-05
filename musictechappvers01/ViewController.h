5b142b00ec0f236df11159f709fd7141413706e4
Jordan Watson <jordanwatson1@dhcp-v207-117.vp.reshsg.uci.edu>
2015-04-05 13:13:49 -0700

    Initial Commit

---
+++ musictechappvers01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  musictechappvers01
+//
+//  Created by Jordan Watson on 4/5/15.
+//  Copyright (c) 2015 Jordan Watson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ musictechappvers01/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  musictechappvers01
+//
+//  Created by Jordan Watson on 4/5/15.
+//  Copyright (c) 2015 Jordan Watson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

cda6caffc5268cb19cf1e78ca015a42c28627a09
jwatson2 <jwatson2@uci.edu>
2015-04-19 14:51:01 -0700

    Initial
    OSC message won’t send to Max

--- musictechappvers01/ViewController.h
+++ musictechappvers01/ViewController.h
@@ -10,6 +10,8 @@
 
 @interface ViewController : UIViewController
 
+- (IBAction)slowerPressed:(UIButton *)sender;
+- (IBAction)fasterPressed:(UIButton *)sender;
 
 @end
 
--- musictechappvers01/ViewController.m
+++ musictechappvers01/ViewController.m
@@ -7,6 +7,16 @@
 //
 
 #import "ViewController.h"
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <ifaddrs.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 @interface ViewController ()
 
@@ -24,4 +34,43 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)slowerPressed:(UIButton *)sender {
+    NSLog(@"slower");
+    // When slower button is pressed, the message slower is sent to the debug window
+    int sock;
+    struct sockaddr_in sa;
+    ssize_t bytes_sent;
+    
+    char            OutBuffer[1024];
+    ssize_t            OutBufferLength;
+    
+    strcpy(OutBuffer,"Hello World!");
+    OutBufferLength = strlen("Hello World!");
+    
+    
+    sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (-1 == sock) /* if socket failed to initialize, exit */
+    {
+        fprintf(stderr,"Error creating socket: %s\n",strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    
+    memset(&sa, 0, sizeof(sa));
+    sa.sin_family = AF_INET;
+    sa.sin_addr.s_addr = htonl(0x7F000001); /* IP address: 127.0.0.1 */
+    sa.sin_port = htons(1337);
+    
+    
+    OutBufferLength = 0;
+    bytes_sent = sendto(sock, OutBuffer, OutBufferLength, 0,(struct sockaddr*)&sa, sizeof (struct sockaddr_in));
+    if (bytes_sent < 0)
+        fprintf(stderr,"Error sending packet: %s\n",strerror(errno));
+    
+    close(sock); /* close the socket */
+}
+
+- (IBAction)fasterPressed:(UIButton *)sender {
+    NSLog(@"faster");
+    // When faster button is pressed, the message faster is sent to the debug window
+}
 @end

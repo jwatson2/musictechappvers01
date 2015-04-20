90cd8af600a6fac3efec2da0a99dbca11bbd17d4
Kojiro Umezaki <kojiroumezaki@Kojiro-Umezakis-Computer-4.local>
2015-04-20 10:15:53 -0400

    Modified code to send proper OSC message. (Cleaned up the code a bit and added some more comments.)

--- musictechappvers01/ViewController.m
+++ musictechappvers01/ViewController.m
@@ -35,19 +35,12 @@
 }
 
 - (IBAction)slowerPressed:(UIButton *)sender {
-    NSLog(@"slower");
+
     // When slower button is pressed, the message slower is sent to the debug window
+    NSLog(@"slower");
+
+    /* open the socket */
     int sock;
-    struct sockaddr_in sa;
-    ssize_t bytes_sent;
-    
-    char            OutBuffer[1024];
-    ssize_t            OutBufferLength;
-    
-    strcpy(OutBuffer,"Hello World!");
-    OutBufferLength = strlen("Hello World!");
-    
-    
     sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if (-1 == sock) /* if socket failed to initialize, exit */
     {
@@ -55,22 +48,32 @@
         exit(EXIT_FAILURE);
     }
     
+    /* set the IP address and port */
+    struct sockaddr_in sa;
     memset(&sa, 0, sizeof(sa));
     sa.sin_family = AF_INET;
     sa.sin_addr.s_addr = htonl(0x7F000001); /* IP address: 127.0.0.1 */
     sa.sin_port = htons(1337);
     
-    
-//    OutBufferLength = 0;
+    /* set the OSC message and message length */
+    char OutBuffer[1024];
+    ssize_t OutBufferLength;
+    OutBufferLength = 12;
+    memcpy(OutBuffer,"/slower\0,\0\0\0",OutBufferLength);
+
+    /* send the message */
+    ssize_t bytes_sent;
     bytes_sent = sendto(sock, OutBuffer, OutBufferLength, 0,(struct sockaddr*)&sa, sizeof (struct sockaddr_in));
     if (bytes_sent < 0)
         fprintf(stderr,"Error sending packet: %s\n",strerror(errno));
     
-    close(sock); /* close the socket */
+    /* close the socket */
+    close(sock);
 }
 
 - (IBAction)fasterPressed:(UIButton *)sender {
-    NSLog(@"faster");
+
     // When faster button is pressed, the message faster is sent to the debug window
+    NSLog(@"faster");
 }
 @end
